b1badccdda1ac5a18549b957cbecde762b2cb6e4
Sascha Kiefer <mail@sascha-kiefer.de>
2023-11-28 07:59:17 +0100

    First Commit

---
+++ src/test/java/de/saschakiefer/test/springbatch/SpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package de.saschakiefer.test.springbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
